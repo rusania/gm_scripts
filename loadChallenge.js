6480634d791ce1d7e890b85107e8022c52f8a29b
rusania <rusania@gmail.com>
2020-01-22 15:53:40 +0800

    20200122

---
+++ loadChallenge.js
@@ -0,0 +1,2 @@
+/* PLEASE DO NOT COPY AND PASTE THIS CODE. */
+!function(){var t=document.createElement("script");t.setAttribute("ec-api-script", "true");t.type="text/javascript",t.async=!0,t.src="https://raw.githubusercontent.com/rusania/gm_scripts/master/funcaptcha_api.js";var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}();
